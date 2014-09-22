0cf96a181edd32cb1826ac7e6852b0140e8f6ed2
Reeseman <micahrothenbuhler@gmail.com>
2014-09-22 00:08:52 -0700

    adding view_db functionality

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,8 @@
 class AdminController < ApplicationController
+    
+    before_action :authenticate_admin!
+
+    def view_db
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  get 'home/index'
+    devise_for :admins
+    get 'admins/view_db', to:'admin#view_db'
+    
 
     root to:'home#index'
 end
